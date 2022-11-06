44150265272182a757a46595994cf0e7ff675d78
Varda666 <oksana.aeshina666@gmail.com>
2022-11-06 18:30:20 +0300

    add

--- get_data.py
+++ get_data.py
@@ -43,4 +43,42 @@ print(len(set(list_new_tokens)))
 print(list_wallets)
 print(list_telegram)
 
+list_price = []
+for var in data1:
+    if "price" in var.keys():
+        price = var["price"]
+        list_price.append(price)
+    else:
+        price = None
+        list_price.append(price)
+
+print(list_price)
+
+list_volume24 = []
+for var in data1:
+    if "volume24h" in var.keys():
+        volume = var["volume24h"]
+        list_volume24.append(volume)
+    else:
+        volume = None
+        list_volume24.append(volume)
+print(list_volume24)
+
+dict_all = {}
+for t in list_tokens:
+    dict_all['token'] = t
+for w in list_wallets:
+    dict_all['wallet'] = w
+for tel in list_telegram:
+    dict_all['telegram'] = tel
+for p in list_price:
+    dict_all['price'] = p
+for v in list_volume24:
+    dict_all['volume'] = v
+
+    print(dict_all)
+
+
+
+
 ''' Достать price, volume 24h. сделать словарь'''
